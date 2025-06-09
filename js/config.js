794ed330b88b945d4109d7fed31ecec1c00cd16f
kvaid <kushagra.vaid@outlook.com>
2025-06-08 20:06:21 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const config = {
     GOOGLE_MAPS_API_KEY: 'AIzaSyBjksFdtZud7U1Y0eZn6jvUJfCk7O1tqjY',
     ONE_BUS_AWAY_API_KEY: '880e54e0-83fc-4c11-bbde-eb74613284d0',
-    UPDATE_INTERVAL: 30000, // 30 seconds
+    UPDATE_INTERVAL: 60000, // 60 seconds
     MAP_CENTER: { lat: 47.6276, lng: -122.2275 }, // Medina, WA
     MAP_ZOOM: 13
 }; 
